3c51be303a27b9e9ece1565a87cc4370d1e5c765
Diego Lopez <awsmr1607@gmail.com>
2018-04-13 12:46:56 -0700

    YEpa STYRL

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
 		System.out.println("Mensaje de Luis");
 		System.out.println("Buenos días, Diego va a suspender programación jejeje.");
 		System.out.println("Esto me lo ha mandau Ana.");
+		System.out.println("Lo hago porque quiero");
 	}
 }
