4daa632fc1cc7ce197ad88bc663b1716eab9b23a
Yeshe S G <yeshe.s.g@gmail.com>
2018-04-27 21:34:03 +0200

    npi

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -2,6 +2,7 @@
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT");
+<<<<<<< master
 		System.out.println("Hello GIIIT");
 		System.out.println("Mensaje de Luis");
 		System.out.println("Buenos días, Diego va a suspender programación jejeje.");
@@ -12,5 +13,7 @@ public class HelloGit {
 
 		System.out.println("Esto me lo ha mandado Ana versión 2.");
 
+=======
+>>>>>>> c3f3768 Version v2
 	}
 }
